ee1a44dcb4673e597f598a4bef2c5deead89ce0e
sj.park <sj.park1@lotte.net>
2022-02-08 21:39:40 +0900

    feat: vue-cli 활용 프로젝트 생성

---
+++ vue-study/src/components/message.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <input type="text" v-model="message" />
+    <div>{{ message }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Message extends Vue {
+  private message: string = '메시지를 입력해라';
+}
+</script>
---
+++ vue-study/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <message></message>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import message from '@/components/message.vue';
+
+@Component({
+  components: {
+    message,
+  },
+})
+export default class Home extends Vue {}
+</script>
