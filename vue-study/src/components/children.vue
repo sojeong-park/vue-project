9dc3d7ad922a406d6eedf0cb3896ca98ada0597a
sj.park <sj.park1@lotte.net>
2022-02-08 22:21:07 +0900

    feat: @Prop() 적용

---
+++ vue-study/src/components/children.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        {{parentMessage}}
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from 'vue-property-decorator'
+
+    @Component
+    export default class Children extends Vue {
+        @Prop() parentMessage?: string;
+    }
+</script>
--- vue-study/src/views/Home.vue
+++ vue-study/src/views/Home.vue
@@ -2,17 +2,22 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <message></message>
+    <children :parentMessage="inputMessage"></children>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import message from '@/components/message.vue';
+import children from '@/components/children.vue';
 
 @Component({
   components: {
     message,
+    children,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  inputMessage: string = '동적데이터 입력';
+}
 </script>
